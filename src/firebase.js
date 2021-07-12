e256b6971fc2c57b602f706129d3856fdf5a0e0c
yogesh chavan <76579825+YOGESH606@users.noreply.github.com>
2021-07-12 22:32:44 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,25 @@
 import firebase from "firebase";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const REACT_APP_API_KEY=process.env.REACT_APP_API_KEY ;
+const REACT_APP_AUTH_DOMAIN=process.env.REACT_APP_AUTH_DOMAIN;
+const REACT_APP_DATABASE_URL= process.env.REACT_APP_DATABASE_URL;
+const REACT_APP_PROJECT_ID=process.env.REACT_APP_PROJECT_ID;
+const REACT_APP_STORAGE_BUCKET= process.env.REACT_APP_STORAGE_BUCKET;
+const REACT_APP_MESSAGING_SENDERID=process.env.REACT_APP_MESSAGING_SENDERID;
+const REACT_APP_APPID=process.env.REACT_APP_APPID;
+const REACT_APP_MEASUREMENT_ID=process.env.REACT_APP_MEASUREMENT_ID;
+
+
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY ,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
-    appId: process.env.REACT_APP_APPID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey:REACT_APP_API_KEY ,
+    authDomain:REACT_APP_AUTH_DOMAIN,
+    databaseURL:REACT_APP_DATABASE_URL,
+    projectId:REACT_APP_PROJECT_ID,
+    storageBucket:REACT_APP_STORAGE_BUCKET,
+    messagingSenderId:REACT_APP_MESSAGING_SENDERID,
+    appId:REACT_APP_APPID,
+    measurementId:REACT_APP_MEASUREMENT_ID
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
